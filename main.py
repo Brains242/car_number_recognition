ef1e964c4e2b0efd65ac50dd8997347b606a963c
Brains242 <alex.brains.242@gmail.com>
2021-08-06 03:18:52 +0300

    Correlation and find colors

--- main.py
+++ main.py
@@ -1,17 +1,58 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('Input/standard_2015.jpg')
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+car_number_path = 'Input/american_2015.jpg'
+lower_blue = [90, 50, 120]
+upper_blue = [138, 255, 255]
+lower_yellow = [22, 93, 0]
+upper_yellow = [45, 255, 255]
 
-lower_range = np.array([90, 150, 0])
-upper_range = np.array([140, 255, 255])
 
-mask = cv2.inRange(hsv, lower_range, upper_range)
-contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+def find_blue(img):
 
-cont = cv2.drawContours(img, contours, -1, (255, 255, 255), 2, cv2.LINE_AA, hierarchy, 1)
-cv2.imshow('Contours', img)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower = np.array(lower_blue, dtype='uint8')
+    upper = np.array(upper_blue, dtype='uint8')
+
+    mask = cv2.inRange(hsv, lower, upper)
+    contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    if len(contours) != 0:
+        c = max(contours, key=cv2.contourArea)
+        x, y, width, height = cv2.boundingRect(c)
+        # cv2.rectangle(img, (x, y), (x + width, y + height), (50, 255, 50), 2)
+        return x, y, width, height
+    return False
+
+
+def find_yellow(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower = np.array(lower_yellow, dtype='uint8')
+    upper = np.array(upper_yellow, dtype='uint8')
+
+    mask = cv2.inRange(hsv, lower, upper)
+    contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    if len(contours) != 0:
+        c = max(contours, key=cv2.contourArea)
+        x, y, width, height = cv2.boundingRect(c)
+        # cv2.rectangle(img, (x, y), (x + width, y + height), (0, 190, 0), 2)
+        return x, y, width, height
+    return False
+
+
+img = cv2.imread(car_number_path)
+x, y, width, height = find_blue(img)
+print(x, y, width/height)
+cv2.rectangle(img, (x, y), (x + width, y + height), (0, 190, 0), 2)
+x, y, width, height = find_yellow(img)
+print(x, y, width/height)
+cv2.rectangle(img, (x, y), (x + width, y + height), (0, 190, 0), 2)
+# cv2.imshow('Image', img)
 while True:
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
